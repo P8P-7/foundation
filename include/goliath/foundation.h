a2fd5f23393a1ebd5633f7d1906173c6e0fffc0d
Amorandron <jan@janwilts.com>
2018-05-18 11:17:03 +0200

    Renamed to foundation

---
+++ include/goliath/foundation.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/**
+ * @file core.h
+ * @author Group 7 - Informatica
+ */
+
+ /**
+  * @namespace goliath::core
+  * @brief Contains base-defenitions every module (and controller) needs
+  */
+
+#include "../../src/handle.h"
+#include "../../src/repositories/repository.h"
+#include "../../src/repositories/proto_repository.h"
+#include "../../src/repositories/battery_repository.h"
+#include "../../src/repositories/watcher.h"
+#include "../../src/publisher_service.h"
